35d66fd4921e1d6d2cbd845041511c8255aa6e99
HotKurry <ggvigeant@gmail.com>
2021-03-12 20:05:07 -0500

    updates to buttons and boxes

--- src/components/BackgroundVideo/BackgroundVideo.js
+++ src/components/BackgroundVideo/BackgroundVideo.js
@@ -3,17 +3,28 @@ import React from 'react';
 import classes from './BackgroundVideo.module.css';
 
 const BackgroundVideo = () => {
-    const videoSource = './choice4.mp4'
+    const VIDEOS = {
+        choice: './choice4.mp4',
+        poorly: 'https://content.codecademy.com/courses/React/react_video-slow.mp4',
+        correct: 'https://content.codecademy.com/courses/React/react_video-cute.mp4'
+      };
+    const LOOP = {
+        onloop: "loop",
+        offloop: "false"
+    };
     return (
         <div className={classes.fullscreen}>
         <div className={classes.Container} >
-            <video autoPlay="autoplay" loop="loop" muted className={classes.Video} >
-                <source src={videoSource} type="video/mp4" />
+            <video autoPlay="autoplay" loop={LOOP.onloop} muted className={classes.Video} >
+                <source src={VIDEOS.choice} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
 
             <div className={classes.Content}>
-                <div className={classes.SubContent} >
+                <div className={classes.LeftContent} >
+
+                </div>
+                <div className={classes.RightContent} >
 
                 </div>
             </div>
